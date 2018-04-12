d9aa0d9d7fa93715bdf8424931564289fe0aa423
Taras MASLYANKO <tmaslyan@e2r12p7.unit.ua>
2018-04-12 20:05:13 +0300

    wothout leaks

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,14 @@
 /*   By: tmaslyan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/03 16:16:18 by tmaslyan          #+#    #+#             */
-/*   Updated: 2018/04/09 21:44:53 by tmaslyan         ###   ########.fr       */
+/*   Updated: 2018/04/12 19:50:29 by tmaslyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
-t_bufer	*do_smth(t_bufer **list, int fd)
+t_bufer	*get_list(t_bufer **list, int fd)
 {
 	if (!(*list))
 	{
@@ -40,7 +41,7 @@ t_bufer	*do_smth(t_bufer **list, int fd)
 	return (*list);
 }
 
-int		check_smth(t_bufer **list, char **res, char *buf)
+int		add_to_line(t_bufer **list, char **res, char *buf)
 {
 	char *temp;
 	char *pointer;
@@ -50,25 +51,26 @@ int		check_smth(t_bufer **list, char **res, char *buf)
 	if ((temp = ft_strchr(buf, '\n')) == NULL)
 	{
 		(*res) = ft_strjoin(*res, buf);
+		free((*list)->buf);
 		(*list)->buf = NULL;
 	}
 	else
 	{
 		pointer = ft_strsub(buf, 0, temp - buf);
 		(*res) = ft_strjoin(*res, pointer);
-		temp++;
-		if (*temp == '\0')
+		free(pointer);
+		pointer = (*list)->buf;
+		if (*(++temp) == '\0')
 			(*list)->buf = NULL;
 		else
 			(*list)->buf = ft_strdup(temp);
 		free(pointer);
-		*pointer = 0;
 		return (1);
 	}
 	return (0);
 }
 
-int		do_smth2(t_bufer **s_list, t_bufer **list, char **line, int fd)
+int		valid_check(t_bufer **s_list, t_bufer **list, char **line, int fd)
 {
 	t_bufer	*temp;
 	char	buf[1];
@@ -76,7 +78,7 @@ int		do_smth2(t_bufer **s_list, t_bufer **list, char **line, int fd)
 	temp = NULL;
 	if (*s_list)
 		temp = *s_list;
-	if (fd < 0 || !(*list = do_smth(s_list, fd))
+	if (fd < 0 || !(*list = get_list(s_list, fd))
 		|| read(fd, buf, 0) < 0 || line == NULL)
 		return (-1);
 	if (temp)
@@ -84,28 +86,49 @@ int		do_smth2(t_bufer **s_list, t_bufer **list, char **line, int fd)
 	return (0);
 }
 
-int		get_next_line(const int fd, char **line)
+int		check_buf(t_bufer **list, char **line, char *buf, int fd)
 {
-	char			*buf;
-	static	t_bufer	*s_list = NULL;
-	int				read_bytes;
-	t_bufer			*list;
+	int read_bytes;
 
-	if (!(buf = (char *)malloc(sizeof(char) * (BUFF_SIZE + 1)))
-		|| do_smth2(&s_list, &list, line, fd) == -1)
-		return (-1);
-	*line = NULL;
-	if (check_smth(&list, line, list->buf))
-		return (1);
 	while ((read_bytes = read(fd, buf, BUFF_SIZE)))
 	{
 		if (read_bytes < 0)
+		{
+			free(buf);
 			return (-1);
+		}
 		buf[read_bytes] = '\0';
-		if (check_smth(&list, line, buf))
+		if (add_to_line(list, line, buf))
 			break ;
 	}
 	if (!read_bytes && !(*line))
 		return (0);
 	return (1);
+}
+
+int		get_next_line(const int fd, char **line)
+{
+	char			*buf;
+	static	t_bufer	*s_list = NULL;
+	t_bufer			*list;
+	int				check;
+
+	if (!(buf = (char *)malloc(sizeof(char) * (BUFF_SIZE + 1)))
+		|| valid_check(&s_list, &list, line, fd) == -1)
+		return (-1);
+	*line = NULL;
+	if (add_to_line(&list, line, list->buf))
+	{
+		free(buf);
+		return (1);
+	}
+	if ((check = check_buf(&list, line, buf, fd)) == -1)
+	{
+		free(buf);
+		return (-1);
+	}
+	free(buf);
+	if (check == 1)
+		return (1);
+	return (0);
 }
